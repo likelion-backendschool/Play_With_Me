95cafbada11281555ab896b4844504db559786f8
SeungKeun Choi <77659341+ddackkeun@users.noreply.github.com>
2022-08-08 04:17:37 +0900

    chore : 프로젝트 생성 및 설정
    의존성 및 프로젝트 세팅 완료
    테스트 연결을 위한 TestController, test.html 추가
    
    확인사항: database 미연결 상태, 연결 필요

---
+++ playwithme/src/main/java/com/idea5/playwithme/PlaywithmeApplication.java
@@ -0,0 +1,14 @@
+package com.idea5.playwithme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class PlaywithmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaywithmeApplication.class, args);
+	}
+
+}
