8671fc7a4c9a1b6f679e94237d2005f5d74b10e3
qkrtpgh5033 <qkrtpgh5033@gmail.com>
2022-09-07 01:02:55 +0900

    feat : 모집완료 시, 동행 테이블에 저장 구현

--- playwithme/src/main/java/com/idea5/playwithme/together/controller/TogetherController.java
+++ playwithme/src/main/java/com/idea5/playwithme/together/controller/TogetherController.java
@@ -6,6 +6,7 @@ import com.idea5.playwithme.member.domain.Member;
 import com.idea5.playwithme.member.dto.MemberRecruitDto;
 import com.idea5.playwithme.member.service.MemberService;
 import com.idea5.playwithme.together.dto.TogetherForm;
+import com.idea5.playwithme.together.service.TogetherService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -25,13 +26,12 @@ public class TogetherController {
      * 댓글 리스트들을 중복없이 데이터 뽑기.
      */
 
-//    @ModelAttribute("nicknames")
-//    private Map<Long, String> nicknames(){
-//        Map<>
-//    }
 
     @Autowired
     MemberService memberService;
+
+    @Autowired
+    TogetherService togetherService;
 
     @GetMapping("/recruit/{board_id}/{article_id}/{member_id}")
     public String recruit(@PathVariable("board_id") Long board_id, @PathVariable("article_id") Long articleId, @PathVariable("member_id") Long memberId, Model model) {
@@ -47,8 +47,8 @@ public class TogetherController {
         List<Long> ids = togetherForm.getIds(); // 작성자가 선택한 댓글 작성자들
         ids.add(memberId); // 작성자 아이디
 
-        for (Long id : ids) {
-            System.out.println("id = " + id);
+        for(int i = 0; i < ids.size(); i++){
+            togetherService.save(articleId, ids.get(i));
         }
 
 
