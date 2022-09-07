c6d42148f3bfd85505ee4d8ea89e078daeeb753c
Ju, Minji <alswl0w0@gmail.com>
2022-09-07 11:06:26 +0900

    refactor : MemberController에서 MyPageController로 타임라인 로직 분리

--- playwithme/src/main/java/com/idea5/playwithme/member/controller/MemberController.java
+++ playwithme/src/main/java/com/idea5/playwithme/member/controller/MemberController.java
@@ -1,31 +1,19 @@
 package com.idea5.playwithme.member.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-
-import com.idea5.playwithme.article.domain.Article;
-import com.idea5.playwithme.board.domain.Board;
-import com.idea5.playwithme.event.domain.Event;
-import com.idea5.playwithme.event.service.EventService;
 import com.idea5.playwithme.member.domain.Member;
 import com.idea5.playwithme.member.dto.KakaoUser;
 import com.idea5.playwithme.member.exception.MemberNotFoundException;
 import com.idea5.playwithme.member.service.KakaoService;
 import com.idea5.playwithme.member.service.MemberService;
-import com.idea5.playwithme.together.domain.Together;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
-import java.security.Principal;
-import java.sql.Date;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
-import java.util.*;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Controller
@@ -34,9 +22,6 @@ import java.util.stream.Collectors;
 public class MemberController {
     private final MemberService memberService;
     private final KakaoService kakaoService;
-    private final EventService eventService;
-
-    private List<Together> togethers = new ArrayList<>();
 
 
     @GetMapping("/login/oauth/kakao/callback")
@@ -101,60 +86,4 @@ public class MemberController {
         return "redirect:/";
     }
 
-    //@PreAuthorize("isAuthenticated()")
-    @GetMapping("/mypage/timeline")
-    public String showTimeline(Model model, Principal principal) {
-
-        if (principal == null) {
-            log.info("You are Logged out. Login or Signin Please!");
-            return "redirect:/";
-        }
-
-        // 현재 로그인한 회원 리턴
-        Member member = memberService.findMember(principal.getName());
-
-        // 해당 회원의 Together 리스트
-        togethers = member.getTogetherList();
-
-        // 위 리스트에서 게시글 객체만 추출한 리스트
-        List<Article> memberArticleList = togethers.stream().map(Together::getArticle).collect(Collectors.toList());
-
-        // 위 리스트에서 게시판 객체만 추출한 리스트
-        List<Board> memberArticleBoardList = memberArticleList.stream().map(Article::getBoard).collect(Collectors.toList());
-
-        // 위 리스트에서 이벤트 id만 추출한 리스트
-        List<Long> memberEventIdList = memberArticleBoardList.stream().map(Board::getId).collect(Collectors.toList());
-
-        // 위 리스트 id에 해당하는 이벤트들 이벤트 리스트에 추가
-        List<Event> events = new ArrayList<>();
-        for (Long t : memberEventIdList) {
-            Event event = eventService.getEvent(t);
-            events.add(event);
-        }
-
-        // 날짜(LocalDateTime) 내림차순 정렬 (최신 날짜가 상단에 오도록)
-        Collections.sort(events, new Comparator<Event>() {
-            @Override
-            public int compare(Event o1, Event o2) {
-                if(o1.getDate().isAfter(o2.getDate())){
-                    return -1;
-                } else if(o1.getDate().isBefore(o2.getDate())){
-                    return 1;
-                } else {
-                    return 0;
-                }
-            }
-        });
-
-
-        model.addAttribute("events", events);
-
-        return "timeline";
-    }
-
-
-
 }
-
-
-
