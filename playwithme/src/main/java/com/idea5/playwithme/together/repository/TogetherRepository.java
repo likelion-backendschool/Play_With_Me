1b91c9c56c87c59f29259acd436a36597a500e53
ahah525 <ahah525@naver.com>
2022-09-13 18:04:10 +0900

    refactor : 기존에 작업하던 회원 평가 together 삭제

--- playwithme/src/main/java/com/idea5/playwithme/together/repository/TogetherRepository.java
+++ playwithme/src/main/java/com/idea5/playwithme/together/repository/TogetherRepository.java
@@ -3,9 +3,6 @@ package com.idea5.playwithme.together.repository;
 import com.idea5.playwithme.together.domain.Together;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface TogetherRepository extends JpaRepository<Together, Long> {
-    // 해당 게시글 id의 동행 목록 중에서 현재 로그인한 회원빼고 조회
-    List<Together> findByArticle_IdAndMember_IdNotLike(long articleId, long memberId);
+
 }
