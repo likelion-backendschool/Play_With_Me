eb649f8bd574dca398c667938055be28ac9b953b
ahah525 <ahah525@naver.com>
2023-03-15 17:49:50 +0900

    #32 refactor: application-secret.yml 에서 중요 데이터 보관, 사용"

--- playwithme/src/main/java/com/idea5/playwithme/KakaoAddress.java
+++ playwithme/src/main/java/com/idea5/playwithme/KakaoAddress.java
@@ -1,18 +1,32 @@
 package com.idea5.playwithme;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component("KakaoAddress")
 public class KakaoAddress {
     private static String now;
 
+    private String host = "https://kauth.kakao.com";
+    private String loginRequestUri = "/oauth/authorize";
+    private String logoutRequestUri = "/oauth/logout";
+
+    @Value("${oauth2.kakao.rest-api-key}")
+    private String restApiKey;
+
+    @Value("${oauth2.kakao.login.redirect-uri}")
+    private String loginRedirectUri;
+
+    @Value("${oauth2.kakao.logout.redirect-uri}")
+    private String logoutRedirectUri;
+
     public String kakaoLogin(String now){
         this.now = now;
-        return "https://kauth.kakao.com/oauth/authorize?client_id=0c7de4f37c334c2d42105c51f0b9ab12&redirect_uri=http://localhost:8080/member/login/oauth/kakao/callback&response_type=code";
+        return "%s%s?client_id=%s&redirect_uri=%s&response_type=code".formatted(host, loginRequestUri, restApiKey, loginRedirectUri);
     }
     public String kakaoLogout(String now){
         this.now = now;
-        return "https://kauth.kakao.com/oauth/logout?client_id=0c7de4f37c334c2d42105c51f0b9ab12&logout_redirect_uri=http://localhost:8080/member/logout/oauth/kakao";
+        return "%s%s?client_id=%s&logout_redirect_uri=%s".formatted(host, logoutRequestUri, restApiKey, logoutRedirectUri);
     }
 
     public static String getNow() {
